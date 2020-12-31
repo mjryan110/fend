555f85b997a094c1f9f7981361de08eafbd1f04a
Michael <mjryan110@gmail.com>
2020-12-30 18:00:28 -0600

    first commit

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,7 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+//console.log(checkForName);
 
-alert("I EXIST")
-console.log("CHANGE!!");
+//alert("I EXIST")
+//console.log("CHANGE!!");
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
