4fd5a25071dbe9ffcbc4a66f6b7d8c9e489ae094
Michael <mjryan110@gmail.com>
2021-01-03 20:29:18 -0600

    testing on getting polarity

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,14 +3,44 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    
+    
+    if (Client.checkForURL((formText)) {
+        console.log("::: Form Submitted :::")
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+        postData('http://localhost:8080/test')
+
+        .then(function(res) {
+        document.getElementById('polarity').innerHTML = 'Polarity: ' + polarityChecker(res.scrore_tag);
+        })
+
+    }   
+
+}
+
+const polarityChecker = (score) => {
+    let display;
+    switch (score) {
+        case 'P+':
+            display = 'strong positive';
+            break;
+        case 'P':
+            display = 'positive';
+            break;
+        case 'NEU':
+            display = 'neutral';
+            break;
+        case 'N':
+            display = 'negative';
+            break;
+        case 'N+':
+            display = 'strong negative';
+            break;
+        case 'NONE':
+            display = 'without sentiment';
+    }
+    return display.toUpperCase();
 }
 
 export { handleSubmit }
+export { polarityChecker }
